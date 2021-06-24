f5a1b89a8b0b03abac6b4de2b28c02f42daf8f2d
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-24 17:53:47 +0800

    Initial Commit

---
+++ SwiftUI FirebaseAuth/SwiftUI_FirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirebaseAuthApp.swift
+//  SwiftUI FirebaseAuth
+//
+//  Created by Amben on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
